3d505f7f07ea6a68e07eaabe0b34aadaf16e0484
Alexandre D. Díaz <dev@redneboa.es>
2026-01-11 06:48:23 +0100

    imp: entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 [ -f "$HOME/.nvm/nvm.sh" ] && . "$HOME/.nvm/nvm.sh"
 
 # Use system python environment
-.  ~/.venv/bin/activate
+. "$HOME/.venv/bin/activate"
 echo "[entrypoint] ==== SYSTEM ENV. INFO ===="
 INFO_ACTIVE_USER=$(whoami 2>&1)
 INFO_ACTIVE_USER_ID=$(id -u 2>&1)
@@ -13,7 +13,7 @@ INFO_ACTIVE_USER_GID=$(id -g 2>&1)
 echo "[entrypoint] - Active user: $INFO_ACTIVE_USER ($INFO_ACTIVE_USER_ID:$INFO_ACTIVE_USER_GID)"
 INFO_PYTHON_VERSION=$(python --version 2>&1)
 echo "[entrypoint] - Python version: $INFO_PYTHON_VERSION"
-if [ -f "~/.nvm/nvm.sh" ]; then
+if [ -f "$HOME/.nvm/nvm.sh" ]; then
     INFO_NODE_VERSION=$(node --version 2>&1)
     echo "[entrypoint] - Node version: $INFO_NODE_VERSION"
 fi
@@ -24,14 +24,14 @@ fi
 echo "[entrypoint] ==== END SYSTEM INFO ===="
 if [ -n "$USER_ODOO_UID" ] && [ -n "$USER_ODOO_GID" ]; then
     echo "[entrypoint] Using custom UID ($USER_ODOO_UID) and GID ($USER_ODOO_GID)..."
-    groupmod -g $USER_ODOO_GID odoo
-    usermod -u $USER_ODOO_UID -g $USER_ODOO_GID odoo
+    groupmod -g "$USER_ODOO_GID" odoo
+    usermod -u "$USER_ODOO_UID" -g "$USER_ODOO_GID" odoo
     chown -R odoo:odoo /home/odoo /opt/odoo /etc/odoo /var/lib/odoo
 fi
 echo "[entrypoint] Generating Odoo configuration..."
 generate_config /etc/odoo/odoo.conf
 echo "[entrypoint] Waiting for postgres database..."
-wait_for_psql --db_host=$OCONF_db_host --db_port=${OCONF_db_port:-5432} --db_user=${OCONF_db_user} --db_password=${OCONF_db_password} --timeout=30
+wait_for_psql --db_host="$OCONF_db_host" --db_port="${OCONF_db_port:-5432}" --db_user="${OCONF_db_user}" --db_password="${OCONF_db_password}" --timeout=30
 deactivate
 
 # Use odoo python environment
